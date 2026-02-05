977d33df51b04795ca7682c35892aa9e9f1d1b84
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-05 05:19:25 +0000

    feat: support commands

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,44 @@ const client = <Client> new Discord.Client({
 });
 
 client.commands = new Discord.Collection();
-client.aliases = <Client["aliases"]> Object.fromEntries(
-    [
-        Discord.ApplicationCommandType.ChatInput,
-        Discord.ApplicationCommandType.User,
-        Discord.ApplicationCommandType.Message,
-        Discord.ApplicationCommandType.PrimaryEntryPoint,
-        "prefixed"
-    ].map(i => [ i, new Discord.Collection() ])
-);
+client.aliases = <Client["aliases"]> Object.fromEntries([
+    Discord.ApplicationCommandType.ChatInput,
+    Discord.ApplicationCommandType.User,
+    Discord.ApplicationCommandType.Message,
+    Discord.ApplicationCommandType.PrimaryEntryPoint,
+    "prefixed"
+].map(i => [ i, new Discord.Collection() ]));
 
-FileSystem.readFile(Path.resolve("./config/secret.yaml"), "utf8").then(file => {
-    client.secret = YAML.parse(file);
+(async () => {
+    await FileSystem.readFile(
+        Path.resolve("./config/secret.yaml"), "utf8"
+    ).then(file => {
+        client.secret = YAML.parse(file);
+    });
 
-    console.log(client.secret);
-});
+    const commandPath = Path.resolve("./dist/command/");
+    FileSystem.readdir(commandPath, { recursive: true }).then(files =>
+        files.filter(file => file.match(/command\.js$/)).forEach(file =>
+            getImport(commandPath, file).then((
+                command: Client['commands'] extends
+                    Discord.Collection<string, infer T> ? T : never
+            ) => {
+                if (command.meta) {
+                    client.commands.set(command.meta.name, command);
+                    Object.entries(command.meta.types).forEach(
+                        ([ type, { alias }]) =>
+                        client.aliases[type as keyof Client["aliases"]]
+                            .set(alias, command.meta.name)
+                    )
+                }
+            })
+        )
+    );
+})();
+
+async function getImport(dir: string, file: string) {
+    const path = Path.join(dir, file);
+    const url = new URL("file://" + path).href;
+    // TODO: adjust url until as simple as possible
+    return await import(url);
+}
