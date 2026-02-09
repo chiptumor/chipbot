a72b9899aee82f4426e9b1df305cbfb9b1d3387e
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-09 09:45:42 -0600

    feat: update client interface

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,27 +1,20 @@
 import * as Discord from "discord.js";
+import type { InteractionTypeInfo } from "./command.ts";
 
 export interface Client extends Discord.Client {
     aliases: {
         interaction: {
             [Discord.InteractionType.ApplicationCommand]: {
-                [K in Discord.ApplicationCommandType]: Discord.Collection<string, string>;
+                [K in InteractionTypeInfo[Discord.InteractionType.ApplicationCommand]]:
+                    Discord.Collection<string, string>;
             };
-            [Discord.InteractionType.MessageComponent]: {};
+            [Discord.InteractionType.MessageComponent]: {
+                [K in InteractionTypeInfo[Discord.InteractionType.MessageComponent]]:
+                    Discord.Collection<string, string>;
+            };
+            [Discord.InteractionType.ModalSubmit]:
+                Discord.Collection<string, string>;
         };
         // [K in keyof InteractionTypeInfo]: Discord.Collection<string, string>;
-    };
-    secret: { [K: string]: any; } & {
-        discord: {
-            token: string;
-            client: Discord.Snowflake;
-            admin: { [K: string]: Discord.Snowflake; };
-            server: {
-                [K: string]: {
-                    id: Discord.Snowflake;
-                    channel: { [K: string]: Discord.Snowflake; };
-                };
-            };
-            user: { [K: string]: Discord.Snowflake; };
-        };
     };
 }
