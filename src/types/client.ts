d6b3bd346535220d60f89213048a578a21a75401
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-02-09 09:58:09 -0600

    feat: create clientaliases

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,23 +1,6 @@
 import * as Discord from "discord.js";
-import type { InteractionTypeInfo } from "./command.ts";
+import type { InteractionTypeInfo, ClientAliases } from "./command.ts";
 
 export interface Client extends Discord.Client {
-    aliases: {
-        interaction: {
-            [Discord.InteractionType.ApplicationCommand]: {
-                [K in InteractionTypeInfo[Discord.InteractionType.ApplicationCommand]]:
-                    Discord.Collection<string, string>;
-            };
-            [Discord.InteractionType.MessageComponent]: {
-                [K in InteractionTypeInfo[Discord.InteractionType.MessageComponent]]:
-                    Discord.Collection<string, string>;
-            };
-            [Discord.InteractionType.ModalSubmit]:
-                Discord.Collection<string, string>;
-        };
-        event: {
-            [Discord.Events.MessageCreate]:
-                Discord.Collection<string, string>;
-        };
-    };
+    aliases: ClientAliases;
 }
